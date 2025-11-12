6aec5707696b19513fedf45b94dcaabc6d0324a8
sujallchavan <sujallchavan@gmail.com>
2025-11-12 23:05:11 +0530

    12-11-2025

---
+++ Second.java
@@ -0,0 +1,8 @@
+public class Second {
+    private static final char[] myName = null;
+
+    public static void main(String[] args) {
+        Second myName1 = new Second();
+        System.out.println(myName1.myName);
+    }
+}
